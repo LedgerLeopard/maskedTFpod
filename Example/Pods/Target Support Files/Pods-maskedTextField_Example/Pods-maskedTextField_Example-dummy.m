370e6d01bf532be39e57dce6a7c00b03e9a7b47e
iuriigushchin <yurii.gushchin@ledgerleopard.com>
2018-09-20 16:33:20 +0300

    remote add

---
+++ Example/Pods/Target Support Files/Pods-maskedTextField_Example/Pods-maskedTextField_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_maskedTextField_Example : NSObject
+@end
+@implementation PodsDummy_Pods_maskedTextField_Example
+@end
---
+++ Example/Pods/Target Support Files/maskedTextField/maskedTextField-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_maskedTextField : NSObject
+@end
+@implementation PodsDummy_maskedTextField
+@end
